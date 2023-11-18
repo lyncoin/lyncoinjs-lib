919a42fb591607cdf0dc5664cb68b45f1a97ada4
JayBitron <jaybitron@gmail.com>
2023-11-18 21:32:30 +0100

    update max supply for lyncoin

--- src/types.js
+++ src/types.js
@@ -83,7 +83,7 @@ function Signer(obj) {
   );
 }
 exports.Signer = Signer;
-const SATOSHI_MAX = 21 * 1e14;
+const SATOSHI_MAX = 91 * 1e17;
 function Satoshi(value) {
   return exports.typeforce.UInt53(value) && value <= SATOSHI_MAX;
 }
